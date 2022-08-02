68d11f0de57971eebe66f688adf4d16ad193a4ce
thatziv <thatziv@gmail.com>
2022-08-02 15:24:31 -0400

    Updates

---
+++ precache-manifest.d1ae069cf63a5802f7f0bcbe29acd8d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d094b04e50888b3d9699",
+    "url": "./static/css/main.a5ea4f5a.chunk.css"
+  },
+  {
+    "revision": "d094b04e50888b3d9699",
+    "url": "./static/js/main.d094b04e.chunk.js"
+  },
+  {
+    "revision": "361f09ee5c12393f7bcf",
+    "url": "./static/js/1.361f09ee.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "802551906ee9199a7482da8e9edb540a",
+    "url": "./index.html"
+  }
+];
